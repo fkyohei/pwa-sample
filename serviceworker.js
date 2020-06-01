03095282671e46f1ec47e353af4a430d26a31a50
fkyohei <kyohei19921016@gmail.com>
2020-06-01 16:42:57 +0900

    v1.2

--- serviceworker.js
+++ serviceworker.js
@@ -6,4 +6,7 @@ self.addEventListener('activate', function(e) {
     console.log('[ServiceWorker] Activate');
 });
 
-// self.addEventListener('fetch', function(event) {});
+// 中身がなくてもこの記述は必要
+// ないと、"Page does not work offline"となり、Add to homescreenができない
+// 参考: https://pawafuru.com/0407
+self.addEventListener('fetch', function(event) {});
