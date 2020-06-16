3c17eb3ba8f5e77a59c6748adc681654e76ecfe9
fkyohei <kyohei19921016@gmail.com>
2020-06-16 13:08:38 +0900

    v2.1 api request test

--- index.js
+++ index.js
@@ -7,3 +7,30 @@ if ('serviceWorker' in navigator) {
             console.log(`Error: ${error}`);
         });
 }
+
+// see also
+// dog-api
+// https://dog.ceo/dog-api/documentation/
+const url = 'https://dog.ceo/api/breeds/image/random';
+
+fetch(url)
+    .then(response => {
+        if (response.ok) {
+            return response.text();
+        }
+        return Promise.reject(new Error('エラーです！'));
+    })
+    .then(response =>{
+        let objRes = JSON.parse(response);
+
+        if (objRes.status !== 'success') {
+            return Promise.reject(new Error('エラーです！'));
+        }
+
+        let dog_image = document.getElementById('image-area');
+        if (dog_image) {
+            dog_image.insertAdjacentHTML('afterbegin', '<img src="' + objRes.message + '">');
+        }
+    })
+    .catch(error => console.error('Error:', error));
+    
